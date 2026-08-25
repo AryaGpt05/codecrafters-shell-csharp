b5871a3995f3379d13de1c0e9942bf93b53c81f9
AryaGpt05 <gguptaarya231@gmail.com>
2026-08-25 19:22:21 +0530

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -4,5 +4,7 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+        string input = Console.ReadLine();
+        Console.WriteLine($"{input} not found");
     }
 }
